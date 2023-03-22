6adfa5e3f2c22b7ff82ab22c72acf0e7dd926587
waley-code <walephlp@gmail.com>
2023-03-22 18:42:58 +0000

    completed models/amenity.py tasks

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class model"""
+    if storage_type == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
