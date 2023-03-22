8b2f6d938bfcbdc5f4fcf734f3de1bb9aa3c19d3
waley-code <walephlp@gmail.com>
2023-03-22 18:48:31 +0000

    completed models/user.py tasks

--- models/user.py
+++ models/user.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    if storage_type == "db":
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', cascade="all, delete", backref="user")
+        reviews = relationship('Review', cascade="all, delete", backref="user")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
