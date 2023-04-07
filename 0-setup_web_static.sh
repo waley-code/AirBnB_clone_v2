2925aaca7c2009175c44eb59d2f5d21a1887a242
waley-code <oyewaleadeosun@yahoo.com>
2023-04-07 09:42:50 +0100

    updated task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # checks if nginx is installed
 if [ ! -x "$(command -v nginx)" ]; then
-          sudo apt-get update
-            sudo apt-get install nginx -y
+        sudo apt-get update
+        sudo apt-get install nginx -y
 fi
 sudo mkdir -p /data/web_static/releases/test        
 sudo touch /data/web_static/releases/test/index.html
@@ -14,10 +14,9 @@ echo "\
       <h1>Wale</h1>
     </body>
   </html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server;/alocation /hbnb_static/ {
-            alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo service nginx restart
