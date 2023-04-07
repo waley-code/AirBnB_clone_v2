9c4786547810f0fd92f3e71593c1e474ff6d11dc
waley-code <oyewaleadeosun@yahoo.com>
2023-04-07 02:19:19 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
-#sets up my web server for the deployment of web-static
-#installs nginx if not already installed
-sudo apt-get update
-sudo apt-get install nginx -y
-#create the necessary folders
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-echo "<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset utf-8>
-    <title>Fake html file</title>
-  </head>
-  <body>
-      <p>This is my fake html file</p>
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-#create a symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-#change ownership of the data folder
+# checks if nginx is installed
+if [ ! -x "$(command -v nginx)" ]; then
+          sudo apt-get update
+            sudo apt-get install nginx -y
+fi
+sudo mkdir -p /data/web_static/releases/test        
+sudo touch /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/shared
+
+echo "\
+  <html>
+    <body>
+      <h1>Wale</h1>
+    </body>
+  </html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-#update the nginx configuration
-sudo sed -i '/server_name _;/a \        location /hbnb_static {\n        alias /data/web_static/current/;\n        }' /etc/nginx/sites-enabled/default
+
+sudo sed -i '/listen 80 default_server;/a\
+        location /hbnb_static/{\
+            alias /data/web_static/current/;\
+                }' /etc/nginx/sites-available/default;
+
 sudo service nginx restart
