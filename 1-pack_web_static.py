9055961567092c5cf12e2922ae07659d94b90862
waley-code <oyewaleadeosun@yahoo.com>
2023-04-07 17:24:07 +0100

    test doc 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Creates a .tgz archive compressing its contents web_static folder."""
 from fabric.api import local
-from datetime import datetime
+import time
 
 
 def do_pack():
     """Creates a .tgz archive compressing its contents web_static folder"""
     try:
         local("mkdir -p versions")
-        strap_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        strap_time = time.strftime("%Y%m%d%H%M%S")
         p_name = f"web_static_{strap_time}.tgz"
         tar_cmd = f"tar -cvzf versions/{p_name} web_static"
         local(tar_cmd)
