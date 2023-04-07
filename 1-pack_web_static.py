42d43b759b78ae0e33bb55daec2fb797567acf87
waley-code <oyewaleadeosun@yahoo.com>
2023-04-07 12:22:45 +0100

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,13 +8,12 @@ from fabric.api import local
 # Creates a comperessed_static folder
 def do_pack():
     """Creates a .tgz archive compressing web_static folder"""
-    
-    local("mkdir -p versions")
-    strap_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    p_name = f"web_static_{strap_time}.tgz"
-    tar_cmd = f"tar -cvzf versions/{p_name} web_static"
-    response = local(tar_cmd)
-    if response.failed:
-        return None
-    else:
+    try:
+        local("mkdir -p versions")
+        strap_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        p_name = f"web_static_{strap_time}.tgz"
+        tar_cmd = f"tar -cvzf versions/{p_name} web_static"
+        local(tar_cmd)
         return "versions/{}".format(p_name)
+    except:
+        return None
