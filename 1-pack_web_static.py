04627f2e7746ae233ebca7a4122fcae25d0e27bd
waley-code <oyewaleadeosun@yahoo.com>
2023-04-07 11:50:38 +0100

    added fabfile documentaion style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
+# Fabfile to:
+#    - update the remote system(s) 
+#    - download and install an application
 """Creates an archive compressing web_static folder
         Returns:
             returns none if the file wasn't created and the created
             archive path if true
     """
-
+# Import Fabric's API module
 from datetime import datetime
 from fabric.api import local
 
 
 # Creates a comperessed_static folder
 def do_pack():
-    """Creates an archive compressing web_static folder
-        Returns:
-            returns none if the file wasn't created and the created
-            archive path if true
+    """
+        Creates an archive compressing web_static folder
+            Returns:
+                returns none if the file wasn't created and the created
+                archive path if true
     """
 
     local("mkdir -p versions")
