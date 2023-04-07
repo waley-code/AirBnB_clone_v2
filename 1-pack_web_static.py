5409df5367b8d6074d770e12914a28eabebde348
waley-code <oyewaleadeosun@yahoo.com>
2023-04-07 10:11:49 +0100

    change documented task 0001

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Creates a comperessed_static folder"""
+"""Creates an archive compressing web_static folder
+        Returns:
+            returns none if the file wasn't created and the created
+            archive path if true
+    """
 
 from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
-    """
-    Creates an archive compressing web_static folder
-    Returns:
-        returns none if the file wasn't created and the created
-        archive path if true
-    """
+    #Creates an archive compressing web_static folder
+    #    Returns:
+    #        returns none if the file wasn't created and the created
+    #        archive path if true
 
     local("mkdir -p versions")
     strap_time = datetime.now().strftime("%Y%m%d%H%M%S")
