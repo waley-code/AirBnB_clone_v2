d6a89833ef7967662273f84b83bde092e19f6fc9
waley-code <oyewaleadeosun@yahoo.com>
2023-04-07 02:19:31 +0100

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# creates a zipped file
+
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """
+          Creates a comperessed_static folder
+    """
+    local("mkdir -p versions")
+    strap_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    p_name = f"web_static_{strap_time}.tgz"
+    tar_cmd = f"tar -cvzf versions/{p_name} web_static"
+    response = local(tar_cmd)
+    if response.failed:
+        return None
+    else:
+        return "versions/{}".format(p_name)
