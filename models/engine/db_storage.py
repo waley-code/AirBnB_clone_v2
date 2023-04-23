40867f7b360f58b4451dfe52ec32c9ef19b57429
waley-code <walephlp@gmail.com>
2023-04-23 06:44:05 +0000

    completed flask project

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,3 +71,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def close(self):
+        """removes the private session"""
+        self.__session.remove()
