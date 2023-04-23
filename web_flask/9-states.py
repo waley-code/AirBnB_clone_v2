9ec99f707dd215d7b530ca0b8e999b76a6ed4a08
waley-code <walephlp@gmail.com>
2023-04-23 06:33:31 +0000

    9-states.py

---
+++ web_flask/9-states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """List all states in DBStorage"""
+    states = storage.all(State)
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
